8b640eb3d5beb0c84dd4fadd5419367e0d336965
SMASobur <sikdar.se@gmail.com>
2026-03-19 14:44:23 +0100

    Refactor Main class with methods for order creation and product handling

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,25 +1,64 @@
 package se.lexicon;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    static void main() {
-        System.out.println("Workshop OOP1\n");
 
-        //crating customer
+    public static void main(String[] args) {
+        // 1. Create Customers list
+        Customer anna = new Customer(1, "Anna", "anna@mail.com");
+        Customer ragavi = new Customer(2, "Erik", "erik@lexicon.se");
+        Customer muthana = new Customer(1, "Anna", "anna@mail.com");
+        Customer shamu = new Customer(2, "Erik", "erik@lexicon.se");
 
-        Customer anna = new Customer(01, "Anna", "anna@mail.com");
-        /*System.out.println(anna.getCustomerInfo());
-        anna.setPhoneNumber("0737373700");
-        System.out.println(anna.getCustomerInfo());*/
+        // 2. Define Products list
+        Product p1 = new Product(1, "Monitor", 7199.90);
+        Product p2 = new Product(2, "Pen", 15.0);
+        Product p3 = new Product(3, "Backpack", 450.0);
+        Product p4 = new Product(4, "Water Bottle", 120.0);
+        Product p5 = new Product(5, "Calculator", 250.0);
+        Product p6 = new Product(6, "Headphones", 800.0);
+        Product p7 = new Product(7, "Charger", 200.0);
+        Product p8 = new Product(8, "Mouse", 349.90);
+        Product p9 = new Product(9, "USB Cable", 90.0);
+        Product p10 = new Product(10, "Router", 380.0);
+        Product p11 = new Product(11, "Tpye-C Cable", 180.0);
 
-        //Create an Order and link it to Anna
-        Order orderAnna = new Order(101, anna);
+        // 3. Create Orders using methods
 
-        orderAnna.addProduct(new Product(1, "Mouse", 99.9));
-        orderAnna.addProduct(new Product(1, "Keyboard", 299.9));
-        orderAnna.addProduct(new Product(1, "Pen", 49.9));
+        // Anna's Order with 10 products
+        createOrder(anna, 101, createProductList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
+        // Mutana's Order with a few products
+        createOrder(muthana, 102, createProductList(p6, p8, p9));
+        // Shamu's Order with a few products
+        createOrder(shamu, 103, createProductList(p8, p11, p9, p7));
+        // Ragavi's Order with a few products
+        createOrder(ragavi, 103, createProductList(p3, p10, p8, p1, p5, p6));
+    }
 
 
-        //Show the summary
-        orderAnna.displayOrder();
+
+    // Method to add products to a list
+    private static List<Product> createProductList(Product... products) {
+        List<Product> list = new ArrayList<>();
+        for (Product p : products) {
+            list.add(p);
+        }
+        return list;
+    }
+
+    // method to initialize the Order and display its summary
+    private static void createOrder(Customer customer, int orderId, List<Product> products) {
+
+        Order order = new Order(orderId, customer);
+
+        // Add each product to the order
+        for (Product p : products) {
+            order.addProduct(p);
+        }
+
+        // Display the results as summary
+        order.displayOrder();
     }
 }
