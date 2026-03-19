207e260b9cee34659a5918a5643c2ab1f16c8c1f
SMASobur <sikdar.se@gmail.com>
2026-03-19 11:54:51 +0100

    Order & Product
    Add Order class with product management and total price calculation
    
    Add Product class with validation and encapsulation.

--- src/main/java/se/lexicon/Order.java
+++ src/main/java/se/lexicon/Order.java
@@ -1,4 +1,59 @@
 package se.lexicon;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    //private fields
+    private int orderId;
+    private double totalPrice;
+    private List<Product> products;
+
+
+    //Constructor
+    public Order(int orderId){
+        this.orderId = orderId;
+        this.products = new ArrayList<>();
+    }
+
+    // Add a product to the list
+    public void addProduct(Product p) {
+        if (p != null) {
+            products.add(p);
+        }
+    }
+
+    // Remove a product from the list
+    public void removeProduct(Product p) {
+        products.remove(p);
+    }
+
+    // Calculate total price
+    public double calculateTotal() {
+        double total = 0;
+        for (Product p : products) {
+            total += p.getPrice();
+        }
+        return total;
+    }
+    //Getters & setters
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public List<Product> getProducts() {
+        return new ArrayList<>(products);
+    }
+
+    public void displayOrder() {
+        System.out.println("Order ID: " + orderId);
+        for (Product p : products) {
+            System.out.println( p.getProductInfo());
+        }
+        System.out.println("Total Order Value: SEK " + calculateTotal());
+        System.out.println("------------------------------------------");
+    }
+
+
 }
--- src/main/java/se/lexicon/Product.java
+++ src/main/java/se/lexicon/Product.java
@@ -1,4 +1,47 @@
 package se.lexicon;
 
 public class Product {
+    //Private fields (Encapsulation)
+    private int productId;
+    private String name;
+    private double price;
+
+    // Constructor
+    public Product(int productId, String name, double price) {
+        this.productId = productId;
+        setName(name);
+        setPrice(price);
+    }
+
+    // Getters and Setters
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Product name cannot be empty.");
+        }
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price >= 0) {
+            this.price = price;
+        } else {
+            throw new IllegalArgumentException("Price cannot be negative.");
+        }
+    }
+
+    public String getProductInfo() {
+        return "ID: " + productId + " | Name: " + name + " | Price: SEK " + price;
+    }
 }
